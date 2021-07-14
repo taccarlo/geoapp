bed271a265cb02dfb710a3e26242d8ed70fcac4d
Carlo Tacchella <tacchella.carlo@gmail.com>
2021-07-14 11:41:28 +0200

    [UP] working with tile.statem.com

--- src/Views/Map/Map.js
+++ src/Views/Map/Map.js
@@ -6,26 +6,34 @@ import style from '../index.style';
 import styleMap from './Map.style';
 import strings from '../../Models/Constants/strings.json';
 import MapView, {MAP_TYPES, PROVIDER_DEFAULT} from 'react-native-maps';
+import MapStyle from './Map.style';
 const Map = () => {
   let str = strings.Map[0];
   return (
     <MapView
-      provider={PROVIDER_DEFAULT}
-      mapType={MAP_TYPES.NONE}
-      style={styleMap.map}
       region={{
         latitude: 37.78825,
         longitude: -122.4324,
         latitudeDelta: 0.09,
         longitudeDelta: 0.035,
-      }}>
+      }}
+      provider={null}
+      mapType={MAP_TYPES.NONE}
+      style={{flex: 1}}
+      style={styleMap.map}
+      showsUserLocation>
       <MapView.UrlTile
-        urlTemplate={'http://a.tile.openstreetmap.org/{z}/{x}/{y}.png'}
-        zIndex={-1}
+        urlTemplate="http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg"
         maximumZ={19}
-        flipY={false}
       />
     </MapView>
   );
 };
+/*
+
+      <MapView.UrlTile
+        urlTemplate="http://a.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png"
+        maximumZ={19}
+      />
+*/
 export default Map;
